1bc61b6c2e0151fa60b0ddbe3159b72b764b768a
Radu Dan <radu@full-throttle.ro>
2024-07-18 00:29:22 +0300

    Patching crypto is no longer permitted

--- conftest.js
+++ conftest.js
@@ -1,2 +1,8 @@
-Object.assign(global, require('cf-emu/runtime'))
+let patch = require('cf-emu/runtime')
+try {
+    Object.assign(global, patch)
+} catch(e) {
+    delete patch.crypto
+    Object.assign(global, patch)
+}
 global.self.BLISS_DATA = ' ["text/plain",1,{"test_global_file_binding":1}]AAAv'
